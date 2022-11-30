5223a0601064a05b006a9ef411caa31e5effc3eb
Акрам <115643985+akramblch@users.noreply.github.com>
2022-11-30 16:39:15 +0600

    Add files via upload

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Footer from './Footer/Footer';
+import Header from "./Header/Header";
+
+const Layout = () => {
+    return (
+        <>
+            <Header/>
+           <Outlet/>  
+        </>
+    );
+};
+
+export default Layout;
